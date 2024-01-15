cde5a4f94a9ba08eb1ec86c092b5c3e5aca1b519
김예지 <=>
2024-01-15 15:32:23 +0900

    feat: newtab 기능 주석처리

--- algo-ext/src/manifest.js
+++ algo-ext/src/manifest.js
@@ -38,7 +38,7 @@ export default defineManifest({
     },
   ],
   permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
+  // chrome_url_overrides: {
+  //   newtab: 'newtab.html',
+  // },
 })
