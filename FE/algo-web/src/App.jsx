56388c13a8eb4532fcf76ab184d2a285dcc40d6d
김예지 <=>
2024-01-19 17:30:25 +0900

    feat: 구글 로그인 버튼 추가

--- FE/algo-web/src/App.jsx
+++ FE/algo-web/src/App.jsx
@@ -2,12 +2,10 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const redirect = "http://localhost/login/oauth2/code/kakao";
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=22e6c492ebd402646a35739869230bc9&redirect_uri=${redirect}&response_type=code`
   return (
     <>
-    asdf
-      <a href={url}>로그인</a>
+      <a href="https://localhost/oauth2/authorization/kakao">카카오 로그인</a>
+      <a href="https://localhost/oauth2/authorization/google">구글 로그인</a>
     </>
   )
 }
