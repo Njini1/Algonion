62673847e6d035f5730e127b99c719d5c1373fdc
Sohyeon <no3yho2@naver.com>
2024-01-22 10:49:53 +0900

    feat: Parsing submit code

--- algo-ext/src/contentScript/baekjoon/baekjoon.js
+++ algo-ext/src/contentScript/baekjoon/baekjoon.js
@@ -12,28 +12,28 @@ if (baekjoonUrl) {
     // console.log(baekjoonUrl)
     const username = findUsername();
     const resultUsername = findResultUsername(baekjoonUrl);
-    // console.log("username:", username);
-    // console.log("resultUsername:", resultUsername)
+    console.log("username:", username);
+    console.log("resultUsername:", resultUsername);
     
     if (username === resultUsername) {
         const resultTable = parsingResultTableList(document);
-        const correctResultTable = parsingCorrectResultTableList(resultTable)
-        const correctLastestResult = correctResultTable? correctResultTable[0]:null
+        const correctResultTable = parsingCorrectResultTableList(resultTable);
+        const correctLastestResult = correctResultTable? correctResultTable[0]:null;
         
-        // console.log("resultTable:", resultTable)
-        // console.log("correctResultTable:", correctResultTable)
-        // console.log("correctLastestResult:", correctLastestResult)
+        console.log("resultTable:", resultTable);
+        console.log("correctResultTable:", correctResultTable);
+        console.log("correctLastestResult:", correctLastestResult);
         
-        const problemId = correctLastestResult? correctLastestResult.problemId:null
-        const submissionId = correctLastestResult? correctLastestResult.submissionId:null
-        console.log("problemId:", problemId)
-        console.log("submissionId:", submissionId)
-        console.log("--------------------------")  
+        const problemId = correctLastestResult? correctLastestResult.problemId:null;
+        const submissionId = correctLastestResult? correctLastestResult.submissionId:null;
+        console.log("problemId:", problemId);
+        console.log("submissionId:", submissionId);
+        console.log("--------------------------");
         // 아래는 확인 중인 코드입니다.
 
-        const submission = submissionId? getSubmission(submissionId):null
-        // let submission = getSubmission(submissionId)
+        let submission = submissionId? getSubmission(submissionId):null;
 
-        console.log("submission:", submission)
+        console.log("submission:", submission);
+
     }
 }
--- algo-ext/src/contentScript/baekjoon/submission.js
+++ algo-ext/src/contentScript/baekjoon/submission.js
@@ -6,12 +6,13 @@ export function parsingResultTableList(doc) {
     if (table === null || table === undefined || table.length === 0) return [];
     
     const headers = Array.from(table.rows[0].cells, (x) => convertResultTableHeader(x.innerText.trim()));
-    
+
     const list = [];
     for (let i = 1; i < table.rows.length; i++) {
       const row = table.rows[i];
+  
       const cells = Array.from(row.cells, (x, index) => convertResultTableList(x, headers[index]));
-    
+
       let obj = {};
       obj.elementId = row.id;
       for (let j = 0; j < headers.length; j++) {
@@ -41,41 +42,11 @@ export function parsingCorrectResultTableList(table) {
 }
 
 export async function getSubmission(submissionId) {
-    const res = await fetch(`https://www.acmicpc.net/source/download/53682822`, {
+    const res = await fetch(`https://www.acmicpc.net/source/download/${submissionId}`, {
         method: 'GET',
     });
-    console.log(">>>>", res); 
+    // console.log(res)
     const code = await res.text();
-    console.log("-->>", code); 
-
+    // console.log(code)
     return code;
 }
-
-
-/*
-async function getSubmitCodeById(submissionId) {
-    // getSubmitCodeFromStats 는 백준허브에 제출된 코드를 가져오며
-    // 해당 제출 코드가 없을 시에, 백준에 제출한 코드를 GET 한다.
-    // 해당 코드를 updateSubmitCodeFromStats 로 백준허브에 업데이트한다.
-    // 우리는 fetchSubmitCodeById 만 사용함
-    let code = await getSubmitCodeFromStats(submissionId);
-    if (isNull(code)) {
-      code = await fetchSubmitCodeById(submissionId);
-      updateSubmitCodeFromStats({ submissionId, code }); // not await
-    }
-    console.log("getSubmitCodeById return:", code);
-    return code;
-}
-
-
-async function fetchSubmitCodeById(submissionId) {
-    return fetch(`https://www.acmicpc.net/source/download/${submissionId}`, { method: 'GET' })    
-    .then((res) => {
-        console.log("fetchSubmitCodeById return:", res);    
-        res.text()
-    })
-    .catch((err) => {
-        console.log('fetchSubmitCodeById Error:', err);
-    })
-}
-*/
