a1c14bc08f517b735ca35bbcffb5b5eabcb19ee5
Yejin Eum <linda3534@naver.com>
2024-01-23 13:51:33 +0900

    feat: problem entity and Solved entity ref and manytoone connect

--- algo-be/backend/src/main/java/com/e1i5/backend/domain/problem/entity/Problem.java
+++ algo-be/backend/src/main/java/com/e1i5/backend/domain/problem/entity/Problem.java
@@ -1,42 +1,43 @@
-//package com.e1i5.backend.domain.problem.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AccessLevel;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Table(name = "problem")
-//@Getter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//public class Problem {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long problemIdx;
-//
-//    //TODO 연결은 나중에
-//    @Column
-//    private String siteName;
-//    @Column
-//    private String problemNum;
-//    @Column
-//    private String problemName;
-//    @Column
-//    private String problemLink;
-//    @Column
-//    private String originLevel;
-//    @Column
-//    private String ourLevel;
-//
-//    @Builder
-//    public Problem(long problemIdx, String siteName, String problemNum, String problemName, String problemLink, String originLevel, String ourLevel) {
-//        this.problemIdx = problemIdx;
-//        this.siteName = siteName;
-//        this.problemNum = problemNum;
-//        this.problemName = problemName;
-//        this.problemLink = problemLink;
-//        this.originLevel = originLevel;
-//        this.ourLevel = ourLevel;
-//    }
-//}
+package com.e1i5.backend.domain.problem.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "problem")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Problem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long problemIdx;
+
+//    siteName과 problemNum을 통해 문제 식별
+//    -> nullablem = false 설정
+    @Column(nullable = false)
+    private String siteName;
+    @Column(nullable = false)
+    private String problemNum;
+    @Column
+    private String problemTitle; //문제 이름
+    @Column
+    private String url;
+    @Column
+    private String originLevel;
+    @Column
+    private String ourLevel;
+
+    @Builder
+    public Problem(long problemIdx, String siteName, String problemNum, String problemTitle, String url, String originLevel, String ourLevel) {
+        this.problemIdx = problemIdx;
+        this.siteName = siteName;
+        this.problemNum = problemNum;
+        this.problemTitle = problemTitle;
+        this.url = url;
+        this.originLevel = originLevel;
+        this.ourLevel = ourLevel;
+    }
+}
--- algo-be/backend/src/main/java/com/e1i5/backend/domain/problem/entity/SolvedProblem.java
+++ algo-be/backend/src/main/java/com/e1i5/backend/domain/problem/entity/SolvedProblem.java
@@ -18,23 +18,16 @@ public class SolvedProblem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long solvedProblemIdx;
 
-    @Column
-    private String submissionId; //제출 번호
-    @Column
-    private String problemId; //문제 번호
-    @Column
-    private String problemTitle; //문제 이름
-    @Column
-    private String siteName;
+    @ManyToOne
+    @JoinColumn(name = "problemIdx", nullable = false)
+    private Problem problem;
 
     //TODO: 나중에 유저,문제테이블 연결해야 함 지금은 임의의 값 넣을거임
     @Column
     private long userNo;
 
     @Column
-    private String problemLevel; //problem table로??
-    @Column
-    private String language;
+    private String submissionId; //제출 번호
 
     //@Lob 어노테이션은 DB에서 varchar보다 더 많은 데이터를 담으려고 사용한 거임 코드나 메모는 길어질 수 있으니까
     @Column
@@ -45,6 +38,8 @@ public class SolvedProblem {
     private String memo;
 
     @Column
+    private String language;
+    @Column
     private String memory;
     @Column
     private String runtime;
@@ -52,44 +47,24 @@ public class SolvedProblem {
     private String codeLength;
     @Column
     private LocalDateTime submissionTime; //문제 푼 날짜,시간
-    @Column
-    private String url;
+
 
     @Column
     private boolean visible;
 
     @Builder
-    public SolvedProblem(long solvedProblemIdx,
-                         String submissionId,
-                         String problemId,
-                         String problemTitle,
-                         String siteName,
-                         long userNo,
-                         String problemLevel,
-                         String language,
-                         String submissionCode,
-                         String memo,
-                         String memory,
-                         String runtime,
-                         String codeLength,
-                         LocalDateTime submissionTime,
-                         String url,
-                         boolean visible) {
+    public SolvedProblem(long solvedProblemIdx, Problem problem, long userNo, String submissionId, String submissionCode, String memo, String language, String memory, String runtime, String codeLength, LocalDateTime submissionTime, boolean visible) {
         this.solvedProblemIdx = solvedProblemIdx;
-        this.submissionId = submissionId;
-        this.problemId = problemId;
-        this.problemTitle = problemTitle;
-        this.siteName = siteName;
+        this.problem = problem;
         this.userNo = userNo;
-        this.problemLevel = problemLevel;
-        this.language = language;
+        this.submissionId = submissionId;
         this.submissionCode = submissionCode;
         this.memo = memo;
+        this.language = language;
         this.memory = memory;
         this.runtime = runtime;
         this.codeLength = codeLength;
         this.submissionTime = submissionTime;
-        this.url = url;
         this.visible = visible;
     }
 
