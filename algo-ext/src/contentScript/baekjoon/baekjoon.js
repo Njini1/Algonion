52a873ab7a9ad6a27d793f20e3ae92dccc565c86
Sohyeon <no3yho2@naver.com>
2024-01-24 13:01:01 +0900

    style: Delete unnecessary code (baekjoon)

--- algo-ext/src/contentScript/baekjoon/baekjoon.js
+++ algo-ext/src/contentScript/baekjoon/baekjoon.js
@@ -3,21 +3,17 @@ import { mutationObserver } from "./observer.js"
 
 const url = window.location.href;
 
+console.log("[E1I5] 백준 문제 페이지 입니다");
 
-const baekjoonUrl = url.startsWith("https://www.acmicpc.net/")? url:null;
-if (baekjoonUrl) {
-    console.log("[E1I5] 백준 문제 페이지 입니다");
+const username = findUsername();
+const resultUsername = findResultUsername(url);
 
-    const username = findUsername();
-    const resultUsername = findResultUsername(baekjoonUrl);
-    
-    if (username === resultUsername) {
-        const lastResultCategory = document.querySelector('.result-text');
+if (username === resultUsername) {
+    const lastResultCategory = document.querySelector('.result-text');
 
-        const status = document.querySelector('.result-text').dataset.color;
-		console.log(status);
+    const status = document.querySelector('.result-text').dataset.color;
+    console.log(status);
 
-        mutationObserver(lastResultCategory);
+    mutationObserver(lastResultCategory);
 
-    }
 }
