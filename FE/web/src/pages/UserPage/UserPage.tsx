df561123e50beb467d8bf307637570c3b81cfc26
Sohyeon <no3yho2@naver.com>
2024-01-29 16:49:26 +0900

    feat: getAsset

--- FE/web/src/pages/UserPage/UserPage.tsx
+++ FE/web/src/pages/UserPage/UserPage.tsx
@@ -1,3 +1,4 @@
+import UsernameSetting from '../../components/User/UsernameSetting.tsx'
 import UserBasis from '../../components/User/UserBasis.tsx'
 import UserBar from '../../components/User/UserBar.tsx'
 import classes from "./UserPage.module.scss"
@@ -5,7 +6,7 @@ import classes from "./UserPage.module.scss"
 function UserPage() {
 	return (
     <div className={classes.userPage}>
-      <p>UserPage</p>
+      <UsernameSetting/>
       <UserBasis/>
       <UserBar/>
     </div>
