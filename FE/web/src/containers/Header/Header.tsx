07de1f9a305dfc1491db158498abb2214a15aca9
onebell <jwj970802@gmail.com>
2024-01-29 17:54:46 +0900

    feat: develop header

--- FE/web/src/containers/Header/Header.tsx
+++ FE/web/src/containers/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import {Avatar} from "@nextui-org/react";
 
 import { NavbarMenu, NavbarMenuItem, NavbarMenuToggle, NextUIProvider } from "@nextui-org/react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-import { AcmeLogo } from "./AcmeLogo";
+import getAsset from "../../utils/getAsset";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -28,28 +29,34 @@ function Header() {
             className="sm:hidden"
           />
           <NavbarBrand>
-            <AcmeLogo />
-            <p className="font-bold text-inherit">ACME</p>
+            <Link href="#">
+            <img src={getAsset('logo/icon_circle.svg')} alt="algo_logo_dark" />
+            <img src={getAsset('logo/logo_light.svg')} alt="algo_logo_light"/>
+            </Link>
           </NavbarBrand>
         </NavbarContent>
 
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
           <NavbarItem>
             <Link color="foreground" href="#">
-              Features
+              제출코드
             </Link>
           </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page">
-              Customers
-            </Link>
-          </NavbarItem>
+          
           <NavbarItem>
             <Link color="foreground" href="#">
-              Integrations
+              커뮤니티
             </Link>
           </NavbarItem>
+          
+          <NavbarItem isActive>
+            <Link href="#" aria-current="page">
+              유저검색
+            </Link>
+          </NavbarItem>
+
         </NavbarContent>
+        
         <NavbarContent justify="end">
           <NavbarItem className="hidden lg:flex">
             <Link href="#">Login</Link>
@@ -60,6 +67,18 @@ function Header() {
             </Button>
           </NavbarItem>
         </NavbarContent>
+
+        {/* 프로필 페이지 - 티어에 따라서 프로필 테두리 색 다르게 주기 (예정) */}
+        {/* <div className="flex gap-4 items-center">
+          <Avatar isBordered color="default" src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+          <Avatar isBordered color="primary" src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+          <Avatar isBordered color="secondary" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+          <Avatar isBordered color="success" src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+          <Avatar isBordered color="warning" src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+          <Avatar isBordered color="danger" src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+        </div> */}
+
+        
         <NavbarMenu>
           {menuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
--- FE/web/src/pages/UserPage/UserPage.tsx
+++ FE/web/src/pages/UserPage/UserPage.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import UsernameSetting from '../../components/User/UsernameSetting.tsx'
->>>>>>> feat/web/usernameSetting
 import UserBasis from '../../components/User/UserBasis.tsx'
 import UserBar from '../../components/User/UserBar.tsx'
 import classes from "./UserPage.module.scss"
@@ -9,11 +6,8 @@ import classes from "./UserPage.module.scss"
 function UserPage() {
 	return (
     <div className={classes.userPage}>
-<<<<<<< HEAD
       <p>UserPage</p>
-=======
       <UsernameSetting/>
->>>>>>> feat/web/usernameSetting
       <UserBasis/>
       <UserBar/>
     </div>
