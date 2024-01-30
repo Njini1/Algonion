a75e62fcb6be41449e17f8bac69e8dd7ddd81480
Sohyeon <no3yho2@naver.com>
2024-01-30 09:17:08 +0900

    feat: Feature-usernameSetting UI

--- FE/web/src/App.tsx
+++ FE/web/src/App.tsx
@@ -2,12 +2,19 @@ import './App.css';
 import Header from './containers/Header/Header.tsx';
 import UserPage from './pages/UserPage/UserPage.tsx';
 import Footer from './containers/Footer/Footer.tsx';
+import UsernameSetting from './components/User/UsernameSetting.tsx'
+import AuthPage from './pages/AuthPage/AuthPage.tsx'
 
 const App: React.FC = () => {
   return (
     <div>
       <Header/>
       <div className="page">
+        {/* 1. 비로그인 상태 (로그인-회원가입 페이지) */}
+        {/* <AuthPage/> */}
+        {/* 2. 비정상 로그인 상태 (닉네임 설정 페이지) */}
+        {/* <UsernameSetting/>   */}
+        {/* 3. 정상 로그인 상태 */}
         <UserPage/>
       </div>
       <Footer/>
