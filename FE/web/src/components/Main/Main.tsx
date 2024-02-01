a094f354bed8db14e092d62feeea26a3d5ebab38
onebell <jwj970802@gmail.com>
2024-02-01 12:40:59 +0900

    .

--- FE/web/src/components/Main/Main.tsx
+++ FE/web/src/components/Main/Main.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+// import React from "react";
 
 export default function Main() {
 
   return (
     <div>
-      <h1>모든 코드, 모두 모아</h1>
+      <div>
+        <h1>모든 코드, 모두 모아</h1>
+      </div>
+      <div>
+        <button>
+          구글 로그인
+        </button>
+        <button>
+          카카오 로그인
+        </button>
+      </div>
     </div>
-
   )
 }
--- FE/web/src/pages/MainPage/MainPage.tsx
+++ FE/web/src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,7 @@
+import Main from "../../components/Main/Main"
+
 export default function MainPage() {
   return(
-    <h1>hi</h1>
-  )
+    <Main/>    
+    )
 }
