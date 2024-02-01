016a8d9d8c304490d56582954664f65d078b6cd2
onebell <jwj970802@gmail.com>
2024-02-01 17:58:04 +0900

    feat: login button

--- FE/web/src/components/Main/Main.tsx
+++ FE/web/src/components/Main/Main.tsx
@@ -1,19 +1,25 @@
 // import React from "react";
+// import getAsset from "../../utils/getAsset";
+import GoogleLoginButton from "./Google";
+import KakaoLoginButton from "./Kakao";
+import classes from "./Main.module.scss"; // .module.scss 파일을 import
 
 export default function Main() {
+  
+  // const KAKAO_AUTH_URL = ""
 
   return (
-    <div>
+    <div className={classes.main_container}>
       <div>
         <h1>모든 코드, 모두 모아</h1>
       </div>
-      <div>
-        <button>
-          구글 로그인
-        </button>
-        <button>
-          카카오 로그인
-        </button>
+      <div className={classes["login-buttons-container"]}>
+        <div>
+        <GoogleLoginButton></GoogleLoginButton>
+        </div>
+        <div>
+        <KakaoLoginButton></KakaoLoginButton>
+        </div>
       </div>
     </div>
   )
