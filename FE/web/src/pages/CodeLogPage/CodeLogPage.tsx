766d33322ce2f7caac052178f82b7e18f934c6a6
Sohyeon <no3yho2@naver.com>
2024-02-03 17:45:15 +0900

    chore: 임시 저장

--- FE/web/src/pages/CodeLogPage/CodeLogPage.tsx
+++ FE/web/src/pages/CodeLogPage/CodeLogPage.tsx
@@ -1,20 +1,16 @@
 import Header from '../../containers/Header/Header.tsx';
-import CodeLogProblem from '../../components/CodeLog/CodeLogProblem.tsx'
-import CodeLog from '../../components/CodeLog/CodeLog.tsx'
-import CodeLogMemo from '../../components/CodeLog/CodeLogMemo.tsx'
-
+import CodeLogList from '../../components/CodeLog/CodeLogList.tsx'
 import Footer from '../../containers/Footer/Footer.tsx';
 
 import classes from "./CodeLogPage.module.scss"
+
 
 function CodeLogPage() {
 	return (
     <div className={classes.page}>
       <Header/>
       <div className={classes.codeLogPage}>
-        <CodeLogProblem/>
-        <CodeLog/>
-        <CodeLogMemo/>
+        <CodeLogList/>
       </div>
       <Footer/>
     </div>
