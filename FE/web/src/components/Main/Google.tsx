861d764d327b3967b673567947ca70d7888dae25
fpssj <fpssj@naver.com>
2024-02-03 20:21:22 +0900

    feat: 소셜로그인 버튼에 백엔드 주소 연결

--- FE/web/src/components/Main/Google.tsx
+++ FE/web/src/components/Main/Google.tsx
@@ -1,22 +1,24 @@
 // import {GoogleLogin} from "@react-oauth/google";
 // import {GoogleOAuthProvider} from "@react-oauth/google";
-import classes from './Google.module.scss'
-
+import classes from "./Google.module.scss";
+const url = import.meta.env.VITE_BACK_END + "/api/oauth2/authorization/google";
 
 const GoogleLoginButton = () => {
-  
-    return (
-      <div className={classes.gSignInWrapper}>
-        {/* <span className={classes.label}>Sign in with:</span> */}
-          <div className={`${classes["google-button"]} ${classes.customGPlusSignIn}`} >
-            <span className={classes.icon}></span>
-            <span className={classes.buttonText}>구글 로그인</span>
-          </div>
-        </div>
-    );
+  return (
+    <div className={classes.gSignInWrapper}>
+      {/* <span className={classes.label}>Sign in with:</span> */}
+      <a
+        href={url}
+        className={`${classes["google-button"]} ${classes.customGPlusSignIn}`}
+      >
+        <span className={classes.icon}></span>
+        <span className={classes.buttonText}>구글 로그인</span>
+      </a>
+    </div>
+  );
 };
 
-export default GoogleLoginButton
+export default GoogleLoginButton;
 
 // import {GoogleLogin} from "@react-oauth/google";
 // import {GoogleOAuthProvider} from "@react-oauth/google";
--- FE/web/src/components/Main/Kakao.tsx
+++ FE/web/src/components/Main/Kakao.tsx
@@ -4,11 +4,10 @@
 //   }
 // }
 
-import Kakaoimg from '../../assets/logo/kakaoSymbol.png';
+import Kakaoimg from "../../assets/logo/kakaoSymbol.png";
 import classes from "./Kakao.module.scss"; //
-
+const url = import.meta.env.VITE_BACK_END + "/api/oauth2/authorization/kakao";
 const KakaoLoginButton = () => {
-  
   // const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
   // const REDIRECT_URI = import.meta.env.VITE_KAKAO_URL;
   // const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -21,13 +20,19 @@ const KakaoLoginButton = () => {
   //   alt="카카오 로그인" />
   // </a>
   return (
-    <button className={classes["kakao-button"]}
+    <a
+      href={url}
+      className={classes["kakao-button"]}
       // onClick={loginHandler}
     >
-      <img src={Kakaoimg} alt='Kakao Logo' className='h-4 w-auto inline-block' />
-      <span className='mx-auto'>카카오 로그인</span>
-    </button>
-  )
-}
+      <img
+        src={Kakaoimg}
+        alt="Kakao Logo"
+        className="h-4 w-auto inline-block"
+      />
+      <span className="mx-auto">카카오 로그인</span>
+    </a>
+  );
+};
 
-export default KakaoLoginButton
+export default KakaoLoginButton;
