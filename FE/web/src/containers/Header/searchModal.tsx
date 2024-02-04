3883d96848c7bf5553f2899fdb485e447e66ca3c
fpssj <fpssj@naver.com>
2024-02-04 21:33:30 +0900

    fix: 웹 표준을 지키도록 button tag를 anchor 태그로 변경

--- FE/web/src/containers/Header/searchModal.tsx
+++ FE/web/src/containers/Header/searchModal.tsx
@@ -1,68 +1,77 @@
-import {useState} from 'react'
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Link} from "@nextui-org/react";
+import { useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Link,
+} from "@nextui-org/react";
 import getAsset from "../../utils/getAsset";
-import {MyInput} from "./MyInput"
-import styles from "./searchModal.module.scss"
-import { useNavigate } from "react-router-dom";
+import { MyInput } from "./MyInput";
+import styles from "./searchModal.module.scss";
 
 export default function SearchModal() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const navigate = useNavigate();
-  
-  const [text, newText] = useState(""); // State to store the search query
-  
-  const onChange = (event: { target: { value: string; }; }) => {
-    newText(event.target.value);
-    console.log(text);
-  }
-
-  const goToSearchPage = () => {
-    navigate(`/search/${text}`);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [keyword, setKeyword] = useState(""); // State to store the search query
+  const onChange = (event: { target: { value: string } }) => {
+    setKeyword(event.target.value);
+    console.log(keyword);
   };
 
   return (
     <div className="flex flex-col gap-2">
       <Link onPress={onOpen} color="secondary" className="max-w-fit ">
-        <img src={getAsset('search_icon.svg')} className={styles.img} />
+        <img src={getAsset("search_icon.svg")} className={styles.img} />
         유저검색
-      </Link>    
-        {/* <Button onPress={onOpen}>Open Modal</Button> */}
-        <Modal
-          size="2xl"
-          isOpen={isOpen}
-          placement={"top"}
-          onOpenChange={onOpenChange}
-        >
-
-
+      </Link>
+      {/* <Button onPress={onOpen}>Open Modal</Button> */}
+      <Modal
+        size="2xl"
+        isOpen={isOpen}
+        placement={"top"}
+        onOpenChange={onOpenChange}
+      >
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">유저 이름을 검색해주세요!</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">
+                유저 이름을 검색해주세요!
+              </ModalHeader>
               <ModalBody>
                 <MyInput
                   onChange={onChange}
-                  value={text}
+                  value={keyword}
                   isClearable
                   placeholder="Search..."
                   radius="md"
                   size="md"
-                  startContent={<img src={getAsset('search_icon.svg')} className="text-zinc-500"/>}
+                  startContent={
+                    <img
+                      src={getAsset("search_icon.svg")}
+                      className="text-zinc-500"
+                    />
+                  }
                 />
               </ModalBody>
               <ModalFooter>
                 <Button color="secondary" variant="light" onPress={onClose}>
                   Close
                 </Button>
-                <Button color="secondary" onPress={goToSearchPage}>
+                <Button
+                  href={`/search?q=${keyword}`}
+                  color="secondary"
+                  as={Link}
+                >
                   Search
                 </Button>
               </ModalFooter>
             </>
           )}
         </ModalContent>
-
-        </Modal>
+      </Modal>
     </div>
   );
 }
