561feb5fdd5285fa9fd7de9753c187fffb25a78d
Dachae <no3yho2@naver.com>
2024-02-04 22:58:14 +0900

    feat: Feature codelog memo

--- FE/web/src/components/CodeLog/CodeLogMemo.tsx
+++ FE/web/src/components/CodeLog/CodeLogMemo.tsx
@@ -1,55 +1,35 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import classes from './CodeLogMemo.module.scss'
 
 function CodeLogMemo() {
-  const [text, setText] = useState(''); // 텍스트 값을 저장하는 state
-  const [isEditing, setIsEditing] = useState(false); // 편집 모드 여부를 저장하는 state
+  const [content, setContent] = useState("");
+  const [isEditable, setIsEditable] = useState(false);
 
-  let initialValue = '';
+  const textarea = document.querySelector('textarea')!;
+
   const handleEditClick = () => {
-    setIsEditing(true); // 버튼 클릭 시 편집 모드로 전환
+    setIsEditable(true);
   };
 
-  const handleTextChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setText(e.target.value); // 텍스트 입력 시 state 값 업데이트
+  const handleSaveClick = () => {
+    setIsEditable(false);
   };
 
-  const handleSaveClick = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    // 텍스트 저장 기능 구현
-    // 예시: API 호출 또는 로컬 스토리지에 저장
-    console.log(e);
-    setText(e.target.value);
-    setIsEditing(false); // 편집 모드 종료
-  };
-
-  const handleCancelClick = () => {
-    // 취소 기능 구현
-    setText(initialValue); // 편집 전 텍스트 값으로 되돌림
-    setIsEditing(false); // 편집 모드 종료
-  };
-  
-  return (
-      <div>
-        <div className={classes.memo}>
-        {isEditing ? (
-          <textarea
-            className={classes.textarea}
-            value={text}
-            onChange={handleTextChange}
-            name="text"
-          />
-        ) : (
-          <pre>{text}</pre>
-        )}
-        {isEditing && (
-          <div className={classes.buttons}>
-            <button onClick={handleSaveClick}>확인</button>
-            <button onClick={handleCancelClick}>취소</button>
-          </div>
-        )}
-      </div>
-      <button onClick={handleEditClick}>수정</button>
+  return (    
+    <div>
+      <textarea
+        className={classes.memo}
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+          textarea.style.height = 'auto';
+          textarea.style.height = textarea.scrollHeight + 'px';
+        }}
+        readOnly={!isEditable}
+      />
+      <button onClick={handleEditClick} style={{ display: isEditable ? 'none' : 'block' }} className={classes.editButton}>수 정</button>
+      <button onClick={handleSaveClick} style={{ display: isEditable ? 'block' : 'none' }} className={classes.saveButton}>확 인</button>
     </div>
     )
 }
