eaddc2df843974625ef53b1981127d9b9122a409
Sohyeon <no3yho2@naver.com>
2024-02-06 09:53:26 +0900

    style: 친구 리스트 표 css 변경

--- FE/web/src/components/User/UserFriendList.tsx
+++ FE/web/src/components/User/UserFriendList.tsx
@@ -1,34 +1,67 @@
+
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
 import classes from './UserFriendList.module.scss'
 
+const rows = [
+  {
+    key: "1",
+    name: "Tony Reichert",
+    role: "CEO",
+    status: "Active",
+  },
+  {
+    key: "2",
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    status: "Paused",
+  },
+  {
+    key: "3",
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    status: "Active",
+  },
+  {
+    key: "4",
+    name: "William Howard",
+    role: "Community Manager",
+    status: "Vacation",
+  },
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "role",
+    label: "ROLE",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+];
+
 function UserFriendList() {
-  return (
+	return (
     <div className={classes.page}>
-      <table className={classes.table}>
-      <thead>
-        <tr>
-          <th>Lorem</th><th>Ipsum</th><th>Dolor</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-      </tbody>
-      </table>
+      <Table aria-label="Example table with dynamic content">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
     </div>
   )
 }
   
 export default UserFriendList
+
