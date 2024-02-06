ddc939df1cd8a657fe6e9bb238b5f713e126c942
onebell <jwj970802@gmail.com>
2024-02-06 09:53:33 +0900

    feat: community 중간저장

--- FE/web/src/components/Community/RoomList.tsx
+++ FE/web/src/components/Community/RoomList.tsx
@@ -1,53 +1,71 @@
 // import React from "react";
-import { Link } from "@nextui-org/react";
+import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
 import classes from "./RoomList.module.scss"
 
 export default function RoomList() {
-  return(
-    <div className={classes.product_container}>
-            <div className={classes.product}>
-                <div className={classes.product_img_div}><img src="../../assets/logo/icon_circle.svg" className={classes.product_img}/></div>
-                <h5 className={classes.product_title}> 상품 제목</h5>
-                <p className={classes.product_des}> 상품 내용 요약</p>
-                <div className={classes.product_mon}> 월 : 15,000￦</div>
-                <div className={classes.product_link_div}><Link className={classes.product_link}> 구독하러가기</Link></div>
-            </div>
-            <div className={classes.product}>
-                <div className={classes.product_img_div}><img src="../../assets/logo/icon_circle.svg" className={classes.product_img}/></div>
-                <h5 className={classes.product_title}> 상품 제목</h5>
-                <p className={classes.product_des}> 상품 내용 요약</p>
-                <div className={classes.product_mon}> 월 : 15,000￦</div>
-                <div className={classes.product_link_div}><Link className={classes.product_link}> 구독하러가기</Link></div>
-            </div>
-            <div className={classes.product}>
-                <div className={classes.product_img_div}><img src="../../assets/logo/icon_circle.svg" className={classes.product_img}/></div>
-                <h5 className={classes.product_title}> 상품 제목</h5>
-                <p className={classes.product_des}> 상품 내용 요약</p>
-                <div className={classes.product_mon}> 월 : 15,000￦</div>
-                <div className={classes.product_link_div}><Link className={classes.product_link}> 구독하러가기</Link></div>
-            </div>
-            <div className={classes.product}>
-                <div className={classes.product_img_div}><img src="../../assets/logo/icon_circle.svg" className={classes.product_img}/></div>
-                <h5 className={classes.product_title}> 상품 제목</h5>
-                <p className={classes.product_des}> 상품 내용 요약</p>
-                <div className={classes.product_mon}> 월 : 15,000￦</div>
-                <div className={classes.product_link_div}><Link className={classes.product_link}> 구독하러가기</Link></div>
-            </div>
-            <div className={classes.product}>
-                <div className={classes.product_img_div}><img src="../../assets/logo/icon_circle.svg" className={classes.product_img}/></div>
-                <h5 className={classes.product_title}> 상품 제목</h5>
-                <p className={classes.product_des}> 상품 내용 요약</p>
-                <div className={classes.product_mon}> 월 : 15,000￦</div>
-                <div className={classes.product_link_div}><Link className={classes.product_link}> 구독하러가기</Link></div>
-            </div>
-            <div className={classes.product}>
-                <div className={classes.product_img_div}><img src="../../assets/logo/icon_circle.svg" className={classes.product_img}/></div>
-                <h5 className={classes.product_title}> 상품 제목</h5>
-                <p className={classes.product_des}> 상품 내용 요약</p>
-                <div className={classes.product_mon}> 월 : 15,000￦</div>
-                <div className={classes.product_link_div}><Link className={classes.product_link}> 구독하러가기</Link></div>
-            </div>
-      </div>
-      
-  )
+  const list = [
+    {
+      title: "Orange",
+      img: "/images/fruit-1.jpeg",
+      price: "$5.50",
+    },
+    {
+      title: "Tangerine",
+      img: "/images/fruit-2.jpeg",
+      price: "$3.00",
+    },
+    {
+      title: "Raspberry",
+      img: "/images/fruit-3.jpeg",
+      price: "$10.00",
+    },
+    {
+      title: "Lemon",
+      img: "/images/fruit-4.jpeg",
+      price: "$5.30",
+    },
+    {
+      title: "Avocado",
+      img: "/images/fruit-5.jpeg",
+      price: "$15.70",
+    },
+    {
+      title: "Lemon 2",
+      img: "/images/fruit-6.jpeg",
+      price: "$8.00",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+  ];
+
+  return (
+    <div className={classes.roomContainer}>
+      {list.map((item, index) => (
+        <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              radius="lg"
+              width="100%"
+              alt={item.title}
+              className="w-full object-cover h-[200px]"
+              src={item.img}
+            />
+          </CardBody>
+          <CardFooter className="text-small justify-between">
+            <b>{item.title}</b>
+            <p className="text-default-500">{item.price}</p>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
