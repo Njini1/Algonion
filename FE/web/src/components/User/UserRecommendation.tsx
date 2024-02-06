cca28c4c011430ed8c0f5a781533c3b04e99c2f1
Sohyeon <no3yho2@naver.com>
2024-02-06 17:53:23 +0900

    style: 스타일 수정

--- FE/web/src/components/User/UserRecommendation.tsx
+++ FE/web/src/components/User/UserRecommendation.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 import UserRecommendationItem from "./UserRecommendationItem.tsx";
+import {Button} from "@nextui-org/react";
+import getAsset from "../../utils/getAsset.ts";
+
+
 import classes from './UserRecommendation.module.scss'
 
-export default function UserRecommendation() {;
-  const [isLoading, setIsLoading] = useState(false);
+export default function UserRecommendation() {
+  // const [isLoading, setIsLoading] = useState(false);
   
   // const handleRefresh = async () => {
   //   setIsLoading(true);
@@ -11,15 +15,16 @@ export default function UserRecommendation() {;
 
   return (
     <div className={classes.container}>
-      <p></p>
-    <button
-      className={classes.refreshButton}
-      // onClick={handleRefresh}
-      disabled={isLoading} // Disable button while loading
-    >
-      {/* {isLoading ? 'Refreshing...' : 'Refresh Recommendations'} */}
       다른 문제 추천!
-    </button>
+
+
+    <div className="flex gap-4 items-center">
+     <Button isIconOnly color="secondary" variant="faded" aria-label="refresh">
+     <img src={getAsset('img/refresh.png')} alt="refresh-icon" />
+
+      </Button>    
+    </div>
+
       <div className={classes.items}>
         <UserRecommendationItem />
       </div>
