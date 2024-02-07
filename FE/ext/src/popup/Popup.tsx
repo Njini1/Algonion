30f4499641a81f05c8b23dbb4d5a8c985d1d201e
김예지 <=>
2024-02-07 12:46:18 +0900

    feat: api에서 불러온 username과 스트릭 표시 기능 추가

--- FE/ext/src/popup/Popup.tsx
+++ FE/ext/src/popup/Popup.tsx
@@ -10,22 +10,31 @@ import { axiosInstance } from '../util/http-commons';
 export const Popup = () => {
   const [isStorageLoading, setIsStorageLoading] = useState(true);
   const [accessToken, setAccessToken] = useState(null);
+  const [response, setResponse] = useState();
   useEffect(() => {
     async function getToken() {
       const res = await chrome.storage.sync.get("access_token");
       const accessToken = res.access_token;
-      console.log(accessToken);
       setAccessToken(accessToken);
       setIsStorageLoading(false);
     }
     getToken();
   }, [])
+  useEffect(() => {
+    async function getUserData() {
+      if (!accessToken) return;
+      const { data } = await axios.get('https://algonion.store/api/v1/profile/ext', { headers: { "Authorization": `Bearer ${accessToken}` } });
+      setResponse(data);
+      console.log(data);
+    }
+    getUserData();
+  }, [accessToken])
 
-  const fetcher = (url: string) => axiosInstance().get(url, { headers: { "Authorization": `Bearer ${accessToken}` } }).then(res => res.data);
-  const { data, error, isLoading } = useSWR('/v1/profile/ext', fetcher);
-  if (isLoading || isStorageLoading) return <div></div>
-  if (error) return <LoginPage />
-  return <UserPage data={data} />
+  if (response) {
+    return <UserPage data={response} />
+  }
+  return <></>
+
 };
 
 export default Popup;
