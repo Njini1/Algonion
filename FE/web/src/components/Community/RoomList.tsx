73bd410bfba8c493444d0b06815f64135dc9dfd1
onebell <jwj970802@gmail.com>
2024-02-07 12:40:24 +0900

    feat: change community detail

--- FE/web/src/components/Community/RoomList.tsx
+++ FE/web/src/components/Community/RoomList.tsx
@@ -1,57 +1,75 @@
 import React from 'react';
-import {Card, CardBody, CardFooter, Image, RadioGroup, Radio} from "@nextui-org/react";
+import {Card, CardBody, CardFooter, Image, RadioGroup, Radio, useDisclosure, Button, 
+    Modal, ModalContent, ModalHeader, ModalBody,
+    Input, ModalFooter, Select, SelectItem} from "@nextui-org/react";
 import classes from "./RoomList.module.scss"
 import { MdAdd } from 'react-icons/md';
 // import getAsset from "../../utils/getAsset";
 
 export default function RoomList() {
-    // const getRandomNumber = () => Math.floor(Math.random() * 14) + 1;
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
-    // const randomNumber = getRandomNumber();
-    // const imagePath = getAsset(`Landscape/landscape-${randomNumber}.jpg`)
     const [selectedTag, setSelectedTag] = React.useState('');
 
 
-    const list = [
+    const RoomList = [
     {
+        id: 1,
         title: "BOJ 1016 제곱 ㄴㄴ 수",
         tag: "#도와주세요",  
     },
     {
+        id: 2,
         title: "SWEA 19189 순열의 아름다움",
         tag: "#도와주세요",
 
     },
     {
+        id: 3,
         title: "Pro 160585 혼자서 하는 틱택토",
         tag: "#같이풀어요",
 
     },
     {
+        id: 4,
         title: "Pro 42883 큰 수 만들기",
         tag: "#같이풀어요",
     },
     {
+        id: 5,
         title: "SWEA 15874 흑돌과 백돌",
         tag: "#도와주세요",
     },
     {
+        id: 6,
         title: "BOJ 1372 단어 합치기",
         tag: "#같이풀어요",
     },
     {
+        id: 7,
         title: "BOj 1841 군사 배치",
         tag: "#도와주세요",
     },
     {
+        id: 8,
         title: "Pro 258711 도넛과 막대 그래프",
         tag: "#같이풀어요",
     },
 ];
 
-    const filteredList = selectedTag ? list.filter(item => item.tag === selectedTag) : list;
+    const filteredList = selectedTag ? RoomList.filter(item => item.tag === selectedTag) : RoomList;
 
-  
+    const tags = [
+        {
+            label: "#도와주세요",
+            value: "#도와주세요",
+        },
+        {
+            label: "#같이풀어요",
+            value: "#같이풀어요",
+        }
+    ]
+
     return (
         <div>
             <div className={classes.buttonContainer}>
@@ -59,9 +77,47 @@ export default function RoomList() {
                 커뮤니티 페이지
                 <div>
                     {/* 방 생성 버튼 */}
-                    <button className={classes.createRoom}>
+                    <Button onPress={onOpen} className={classes.createRoom}>
                         <MdAdd/>
-                    </button>
+                        <Modal 
+                        isOpen={isOpen} 
+                        onOpenChange={onOpenChange}
+                        placement="top-center">
+                             <ModalContent>
+                            {(onClose) => (
+                                <>
+                                <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+                                <ModalBody>
+                                    <Input
+                                    autoFocus
+                                    label="문제 정보를 입력하세요"
+                                    placeholder="(ex: Pro 258711 도넛과 막대 그래프)"
+                                    variant="bordered"
+                                    />
+                                    <Select
+                                        label="태그를 골라주세요"
+                                        >
+                                            {tags.map((tag) => (
+                                                <SelectItem key={tag.value} value={tag.value}>
+                                                    {tag.label}
+                                                </SelectItem>
+                                            ))}                    
+                                    </Select>
+
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" variant="flat" onPress={onClose}>
+                                    취소
+                                    </Button>
+                                    <Button color="primary" onPress={onClose}>
+                                    완료
+                                    </Button>
+                                </ModalFooter>
+                                </>
+                            )}
+                            </ModalContent>
+                        </Modal>
+                    </Button>
 
                 </div>
             </div>
@@ -71,7 +127,6 @@ export default function RoomList() {
                 onValueChange={setSelectedTag}
                 orientation="horizontal"
                 color='secondary'
-                
             >
                 <Radio value="">
                     전체 보기
