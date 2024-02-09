f0b8b6cfa78e5ecd065c14eb20846fb259bd921d
김수환 <soohwan4030@gmail.com>
2024-02-09 22:52:14 +0900

    feat: create notion feature

--- algo-be/backend/src/main/java/com/e1i5/backend/domain/user/controller/AuthController.java
+++ algo-be/backend/src/main/java/com/e1i5/backend/domain/user/controller/AuthController.java
@@ -1,24 +1,16 @@
 package com.e1i5.backend.domain.user.controller;
 
-import com.e1i5.backend.domain.user.dto.request.CreateAccessTokenRequest;
 import com.e1i5.backend.domain.user.dto.request.NicknameRequest;
 import com.e1i5.backend.domain.user.dto.response.CreateAccessTokenResponse;
 import com.e1i5.backend.domain.user.exception.UserNotFoundException;
 import com.e1i5.backend.domain.user.service.AuthService;
 import com.e1i5.backend.global.util.CookieUtil;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.UUID;
 
 @CrossOrigin("*")
 @RestController
