1127d3d6fabed29fbda7ce949fad7ca34232604b
fpssj <fpssj@naver.com>
2024-02-12 16:20:51 +0900

    feat: 메인페이지 글자 떠오르는 속도 빠르게 조정

--- FE/web/src/components/Main/Description.tsx
+++ FE/web/src/components/Main/Description.tsx
@@ -1,25 +1,23 @@
 // https://www.framer.com/motion/scroll-animations/
-import { motion } from "framer-motion"
-import classes from "./Description.module.scss"
+import { motion } from "framer-motion";
+import classes from "./Description.module.scss";
 
 // type items = { title: string, description: string[] };
-type items = { items: { title: string; description: string[]; }; }
+type items = { items: { title: string; description: string[] } };
 
 function Description(props: items) {
-
   return (
-    
     <div className={classes.component}>
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: false }}
         transition={{
-            ease: "easeInOut",
-            duration: 1.8,
-            y: { duration: 1 },
+          ease: "easeInOut",
+          duration: 0.5,
+          y: { duration: 0.5 },
         }}
-        >
+      >
         <p className={classes.title}> {props.items.title}</p>
       </motion.div>
 
@@ -29,16 +27,16 @@ function Description(props: items) {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: false }}
         transition={{
-            ease: "easeInOut",
-            duration: 2.2,
-            y: { duration: 1 },
+          ease: "easeInOut",
+          duration: 0.5,
+          y: { duration: 0.5 },
         }}
-        >
-          <p>{props.items.description[0]}</p>
-          <p>{props.items.description[1]}</p>
-        </motion.div>
+      >
+        <p>{props.items.description[0]}</p>
+        <p>{props.items.description[1]}</p>
+      </motion.div>
     </div>
   );
 }
-  
-export default Description
+
+export default Description;
