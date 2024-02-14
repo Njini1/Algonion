d39911a2eee12c5ee7ca47c8fb3fd8f467d97a0e
onebell <jwj970802@gmail.com>
2024-02-14 14:49:45 +0900

    feat: login success page setTimeout

--- FE/web/src/pages/LoginSuccessPage/LoginSuccessPage.tsx
+++ FE/web/src/pages/LoginSuccessPage/LoginSuccessPage.tsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react";
 import { deleteCookie, getCookie } from "../../utils/cookieUtil";
 import {CircularProgress} from "@nextui-org/react";
 import { getNickname } from "../../api/nicknameAPI";
+import { useNavigate } from "react-router-dom";
 
 function LoginSuccessPage() {
+  
+  const navigate = useNavigate();
+  const [isFinished, setIsFinished] = useState(false);
+
+  // access token 가져오기
   useEffect(() => {
     const accessToken = getCookie("access_token");
     if (accessToken) {
       deleteCookie("access_token");
     }
   }, []);
+
+  // nickname 가져오기
   const [nickname, setNickname] = useState("");
-  
   useEffect(() => {
     const fetchData = async () => {
   
@@ -21,14 +28,18 @@ function LoginSuccessPage() {
     };
   
     fetchData();
+
+    setIsFinished(true);
   }, [nickname]);
 
-  // 페이지 접속하자마자 바로 이동
-  window.onload = function () {
-    window.location.replace("/mainpage");
-  };
-
-
+  // 작업이 완료되면 메인 페이지로 이동
+  useEffect(() => {
+    if (isFinished) {
+      setTimeout(() => {
+        navigate("/");
+      }, 2000)
+    }
+  }, [isFinished, navigate]);
   return (
     <div className="flex flex-col items-center justify-center h-screen">
 
