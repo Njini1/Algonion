e79f8f1ed91d2858d7a109f746135dc5405f381e
김예지 <=>
2024-02-15 17:53:59 +0900

    feat: 티어 제대로 뜨게 수정 및 프로그래머스 오류 수정

--- FE/ext/src/popup/components/TierBox.tsx
+++ FE/ext/src/popup/components/TierBox.tsx
@@ -2,12 +2,21 @@ import { Variants, motion } from 'framer-motion';
 import getAsset from '../utils/getAsset';
 import classes from './TierBox.module.scss';
 
-export function TierBox(props: { item: Variants | undefined, nickname?: string }) {
+const tierDic: { [key: number]: string } = {
+  0: 'unranked',
+  1: 'bronze',
+  2: 'silver',
+  3: 'gold',
+  4: 'platinum',
+  5: 'diamond',
+  6: 'master',
+};
+export function TierBox(props: { item: Variants | undefined; nickname?: string; tier: number }) {
   return (
     <motion.div className={classes.box} variants={props.item}>
       {/* <img src={getAsset('tier/star_master.svg')} alt="tier-master" /> */}
       <motion.img
-        src={getAsset('tier/star_master.svg')}
+        src={getAsset(`tier/star-${tierDic[props.tier]}.svg`)}
         initial={{ scale: 0, rotate: -180 }}
         animate={{ rotate: 0, scale: 1 }}
         whileHover={{ scale: 1.2, rotate: 90 }}
@@ -21,7 +30,7 @@ export function TierBox(props: { item: Variants | undefined, nickname?: string }
         <p className={classes.title}>티어</p>
         <div className={classes.content}>
           <p className={classes.username}>{props?.nickname}</p>
-          <p className={classes.tier}>UNRANKED</p>
+          <p className={classes.tier}>{tierDic[props.tier]}</p>
         </div>
       </div>
     </motion.div>
