af09fcf7dd58c19a367531d76a82a5e6356783f8
김예지 <=>
2024-02-15 10:12:26 +0900

    feat: refresh 토큰으로 refresh 한다

--- FE/web/src/utils/instance.ts
+++ FE/web/src/utils/instance.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { setCookie } from "./cookieUtil";
 const api = import.meta.env.VITE_BACK_END;
 
 const accessToken = localStorage.getItem("access_token");
@@ -20,5 +21,32 @@ const axiosApi = (): AxiosInstance => {
   return instance;
 };
 
-export { axiosAuthApi, axiosApi };
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+    if (status === 401) {
+      const originalRequest = config;
+      // token refresh 요청
+      const { data } = await axios.post(
+        `${api}/v1/user/token`,
+        {},
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      // 새로운 토큰 저장
+      localStorage.setItem("access_token", data.access_token);
+      setCookie("refresh_token", data.refresh_token, 0);
+      originalRequest.headers.authorization = `Bearer ${data.access_token}`;
+      // 새로운 토큰으로 재요청
+      return axios(originalRequest);
+    }
+    return Promise.reject(error);
+  }
+);
 
+export { axiosAuthApi, axiosApi };
